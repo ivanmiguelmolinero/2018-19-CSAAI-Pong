ef3609ab3a2995ee97dbffdf3f555aea8bd320c0
ivanmiguelmolinero <i.miguel.2016@alumnos.urjc.es>
2019-03-11 22:36:23 +0100

    Ahora parece un frontón

--- Pong.js
+++ Pong.js
@@ -28,6 +28,11 @@ function main () {
   ctx.font = "50px Comic Sans"
   ctx.fillText("0",3*canvas.width/4, 40)
 */
+
+  var i;
+
+  var choque_bola_raqueta = false;
+
   var bola = {
 
     x_ini : 50,
@@ -36,8 +41,8 @@ function main () {
     x : 0,
     y: 0,
 
-    vx : 15,
-    vy : 15,
+    vx : 7,
+    vy : 7,
 
     ctx : null,
 
@@ -53,19 +58,16 @@ function main () {
     },
 
     init : function(ctx) {
-      console.log("Bola: init")
       this.reset();
       this.ctx= ctx;
     },
 
     draw : function () {
-      console.log("bola: draw")
       this.ctx.fillStyle = 'white';
       this.ctx.fillRect(this.x, this.y, this.width, this.height);
     },
 
     update : function () {
-      console.log("bola: update");
       this.x = this.x + this.vx;
       this.y = this.y + this.vy;
     },
@@ -75,9 +77,58 @@ function main () {
   bola.init(ctx);
   bola.draw();
 
-  var timer = null;
+  var raqueta = {
 
-  var sacar = document.getElementById("sacar");
+    x_ini: 40,
+    y_ini: 40,
+
+    x: 0,
+    y: 0,
+
+    ctx: null,
+
+    vy: 30,
+
+    width: 10,
+    height: 60,
+
+    direccion: null,
+
+    init : function(ctx) {
+      this.x = this.x_ini;
+      this.y = this.y_ini;
+      this.ctx = ctx;
+
+    },
+
+    draw: function() {
+      this.ctx.fillStyle = 'white';
+      this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    },
+
+    update: function() {
+      if (this.direccion == "arriba") {
+        this.y = this.y - this.vy;
+      } else if (this.direccion == "abajo") {
+        this.y = this.y + this.vy;
+      }
+    },
+
+    hit: function(a,b) {
+      for (i = this.y; i < this.y + this.height; i++) {
+        if (a == raqueta.x + raqueta.width && ( b == i || b+1 == i || b+2 ==  i || b+3 ==  i || b+4 ==  i || b+5 ==  i) )  {
+          choque_bola_raqueta = true;
+          console.log("Han chocado");
+        }
+     }
+   }
+
+  }
+
+  raqueta.init(ctx);
+  raqueta.draw();
+
+  var timer = null;
 
   window.onkeydown = (e) => {
     e.preventDefault();
@@ -86,9 +137,21 @@ function main () {
       // Lanzar el timer (si es que no estaba ya lanzado)
       if (!timer) {
         timer = setInterval(() =>{
-          console.log("tic");
           // Actualizar la bola
            bola.update()
+
+         // Movimiento de una raqueta
+         window.onkeydown = (e) => {
+           e.preventDefault();
+
+           if (e.key == 'ArrowUp' && raqueta.y > raqueta.y_ini) {
+             raqueta.direccion = "arriba";
+             raqueta.update()
+           } else if (e.key == "ArrowDown" && raqueta.y + raqueta.height < canvas.height ) {
+             raqueta.direccion = "abajo";
+             raqueta.update();
+           }
+        }
 
           // Borrar el canvas
           ctx.clearRect(0,0,canvas.width, canvas.height);
@@ -96,49 +159,61 @@ function main () {
           //Dibujar la bola
           bola.draw()
 
-          // Condicion de terminacion
+          //Dibujar la raqueta
+          raqueta.draw();
+
+          //Comprobacion de si la bola choca con la raqueta
+          raqueta.hit(bola.x,bola.y)
+
+          // Choque y rebote de la bola
           if (bola.x > canvas.width) {
-            bola.vx = -15;
+            bola.vx = -7;
             bola.direccion = "izquierda";
           } else if (bola.y < 0 && bola.x < 0) {
-            bola.vx = 15;
-            bola.vy= 30;
+            bola.vx = 7;
+            bola.vy= 15;
             bola.direccion = "derecha";
           } else if (bola.x > canvas.width && bola.y > canvas.height) {
-            bola.vx = -30;
-            bola.vy = -15;
+            bola.vx = -15;
+            bola.vy = -7;
             bola.direccion = "izquierda";
           } else if (bola.vx < 0 && bola.vy > canvas.height) {
-            bola.vx = 30;
-            bola.vy = -15;
+            bola.vx = 15;
+            bola.vy = -7;
             bola.direccion = "derecha";
           } else if (bola.x > canvas.width && bola.y < 0) {
-            bola.vx = -15;
-            bola.vy = 30;
+            bola.vx = -7;
+            bola.vy = 15;
             bola.direccion = "izquierda";
           } else if (bola.y > canvas.height) {
             // Controlo la direccion de rebote de la bola
             if (bola.direccion == "izquierda") {
-              bola.vx = -15;
-              bola.vy = -15;
+              bola.vx = -7;
+              bola.vy = -7;
             } else {
-              bola.vx = 15;
-              bola.vy = -15;
+              bola.vx = 7;
+              bola.vy = -7;
             }
           } else if (bola.y < 0) {
             // Controlo la direccion de rebote de la bola
             if (bola.direccion == "izquierda") {
-              bola.vx = -15;
-              bola.vy = 15;
+              bola.vx = -7;
+              bola.vy = 7;
             } else {
-              bola.vx = 15;
-              bola.vy = 15;
+              bola.vx = 7;
+              bola.vy = 7;
             }
           } else if (bola.x < 0) {
-            bola.vx = 15;
+            bola.vx = 7;
             bola.direccion = "derecha";
+          } else if (choque_bola_raqueta) {
+            bola.vx = 7;
+            bola.direccion = "derecha";
+            // Vuelvo a poner el choque en false para poder comprobarlo en cada interaccion
+            choque_bola_raqueta = false;
           }
-        }, 20)
+
+        }, 15)
       }
     }
   }
